dc44e9c5653135b8c8b89609b35d02393990ab59
dianlujitao <dianlujitao@lineageos.org>
2019-10-15 22:10:05 +0530

    sepolicy: make entry of dynamic hals in common

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,11 +3,24 @@
 # inherit from Lineage
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/xtended/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/xtended/sepolicy/common/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/xtended/sepolicy/common/dynamic
+else
+
 BOARD_SEPOLICY_DIRS += \
+    device/xtended/sepolicy/common/dynamic \
     device/xtended/sepolicy/common/vendor
+endif
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -12,8 +12,6 @@ endif
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/xtended/sepolicy/qcom/private
 
-ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/xtended/sepolicy/qcom/vendor
-endif
 
